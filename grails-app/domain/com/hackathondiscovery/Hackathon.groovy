2937ce975e0eb425239943c9c9478f0a44b0f24c
Johanan Lancaon <johanan.lancaon@mercadolibre.com>
2012-09-20 10:21:33 -0700

    Checking in hackathon domain and unit test (initial checkin).

---
+++ grails-app/domain/com/hackathondiscovery/Hackathon.groovy
@@ -0,0 +1,7 @@
+package com.hackathondiscovery
+
+class Hackathon {
+
+    static constraints = {
+    }
+}
---
+++ test/unit/com/hackathondiscovery/HackathonTests.groovy
@@ -0,0 +1,17 @@
+package com.hackathondiscovery
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Hackathon)
+class HackathonTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
